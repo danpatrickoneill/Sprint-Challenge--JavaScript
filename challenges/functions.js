152595f30dcea72701bf70289e8b6291cf0e7081
danpatrickoneill <dan47573@gmail.com>
2019-03-01 11:55:16 -0500

    Finished functions challenges

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(x, y, cb) {
+  return cb(x, y)
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +18,14 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (x, y) => x + y;
+const multiply = (x, y) => x * y;
+const greeting = (x, y) => `Hello ${x} ${y}, nice to meet you!`
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,7 +33,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// Closure! Closure gives functions access to variables defined above their own scope, whether it's a global variable or from a previous function. It's the same reason `myFunction()` can access `external`. But, for instance, putting `console.log(internal)` at the bottom of all this would throw an error, since it doesn't go in the opposite direction.
 
 const external = "I'm outside the function";
 
